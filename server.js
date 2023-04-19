f17674ad1867b5d9ac3450ef7a9871477c045fbb
carliys <cflores1833@yahoo.com>
2023-04-19 13:35:43 -0700

    hmm we are getting somewhere

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const logo = require('asciiart-logo');
 
 const mysql = require('mysql2');
 require('dotenv').config();
@@ -13,6 +12,56 @@ var connection = mysql.createConnection({
     database: 'employees_db'
 
 });
+
+const mainMenu = () => {
+    return inquirer.createPromptModule([{
+
+        name: 'mainMenu',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'view all departments',
+            'view all roles',
+            'view all employees',
+            'add a department',
+            'add a role',
+            'add an employee',
+            'update an employee role',
+            'exit'
+        ]
+    }])
+    .then(userChoice => {
+        switch (userChoice.menu) {
+            case 'view all departments':
+                selectDepartments();
+                    break;
+                case 'view all roles':
+                    selectRoles();
+                    break;
+                case 'view all employees':
+                    selectEmployees();
+                    break;
+                case 'add a department':
+                    promptAddDepartment();
+                    break;
+                case 'add a role':
+                    promptAddRole();
+                    break;
+                case 'add an employee':
+                    promptAddEmployee();
+                    break;
+                case 'update an employee role':
+                    promptUpdateRole();
+                    break;
+                default:
+                    process.exit();
+        }
+    });
+};
+
+
+mainMenu();
+
 
  
 
